2cc062280faf41bdea8bf772f21e7bbbc53e1947
Lilly Daniell <daniell.lilly@gmail.com>
2022-01-14 14:57:09 +0000

    Programme returns mixed amounts of change for all coins and notes

--- lib/change_generator.rb
+++ lib/change_generator.rb
@@ -11,7 +11,13 @@ class ChangeGenerator
     twenty if @total != 0  
     ten if @total != 0  
     five if @total != 0
-    fifty_pence if @total != 0
+    one_pound if @total != 0
+    fifty_pence if @total != 0.0
+    twenty_pence if @total != 0.0
+    ten_pence if @total != 0.0
+    five_pence if @total != 0.0
+    two_pence if @total != 0.0
+    one_pence if @total != 0.0
     @conversion.flatten
   end
 
@@ -45,15 +51,62 @@ class ChangeGenerator
     if @total >= 5
       fives = (@total / 5).floor
       @conversion << ["£5"] * fives
-      @total -= 10 * fives
+      @total -= 5 * fives
+    end
+  end
+
+  def one_pound
+    if @total >= 1.0
+      @conversion << ["£1"] * @total.floor
+      @total -= 1 * @total.floor
     end
   end
 
   def fifty_pence
     if @total >= 0.5
-      fiftypences = (@total / 0.5).floor
-      @conversion << ["50p"] * fiftypences
-      @total -= 0.5 * fiftypences
+      fifty_pences = (@total / 0.5).floor
+      @conversion << ["50p"] * fifty_pences
+      @total -= 0.5 * fifty_pences
+    end
+  end
+
+  def twenty_pence
+    if @total >= 0.2
+      twenty_pences = (@total / 0.2).floor
+      @conversion << ["20p"] * twenty_pences
+      @total -= 0.2 * twenty_pences
+    end
+  end
+
+  def ten_pence
+    if @total >= 0.1
+      ten_pences = (@total / 0.1).floor
+      @conversion << ["10p"] * ten_pences
+      @total -= 0.1 * ten_pences
+    end
+  end
+
+  def five_pence
+    if @total >= 0.05
+      five_pences = (@total / 0.05).floor
+      @conversion << ["5p"] * five_pences
+      @total -= 0.05 * five_pences
+    end
+  end
+
+  def two_pence
+    if @total >= 0.02
+      two_pences = (@total / 0.02).floor
+      @conversion << ["2p"] * two_pences
+      @total -= 0.02 * two_pences
+    end
+  end
+
+  def one_pence
+    if @total >= 0.01
+      one_pences = (@total / 0.01).floor
+      @conversion << ["1p"] * one_pences
+      @total -= 0.05 * one_pences
     end
   end
 end
--- spec/change_generator_spec.rb
+++ spec/change_generator_spec.rb
@@ -15,7 +15,19 @@ describe ChangeGenerator do
     expect(change_generator.convert(75.00)).to eq(["£50", "£20", "£5"])
   end
 
-  it "should return 10p fro 0.10" do
+  it "should return 50p for 0.50" do
     expect(change_generator.convert(0.50)).to eq(["50p"])
+  end
+
+  it "should return 20p for 0.20" do
+    expect(change_generator.convert(0.20)).to eq(["20p"])
+  end
+
+  it "should return £5 £1 £1 £1 for 8.0" do
+    expect(change_generator.convert(8.0)).to eq(["£5", "£1", "£1", "£1"])
+  end
+ 
+  it "should return mixed amounts of change for all coins and notes" do
+    expect(change_generator.convert(76.81)).to eq(["£50", "£20", "£5", "£1", "50p", "20p", "10p", "1p"])
   end
 end
